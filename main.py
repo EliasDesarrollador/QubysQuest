32cfaa37a83c46ff42088e66cf91bbadddc25a26
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-10-04 16:37:50 -0300

    Punto de entrada del juego

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 
+#Punto de entrada del juego 
+#importamos la clase Game  que contiene el loop principal
+from core.game import Game
+
+#Funcion principal
+    def main():
+ #Creamos una instancia  del juego 
+    game  = Game()
+  #EJecutamos el loop principal 
+    game.run()
+  #Esto hace que main () se  ejecute  solo si corremos el archivo correctamente
+    if __name__ == "__main__":
+        main() 
